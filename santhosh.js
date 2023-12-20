99b47a2960d81dacf1b69732643cbfd47606e07c
DCKAP <santhoshkumar.edckap@gmail.com>
2023-12-20 17:59:06 +0530

    files added

--- santhosh.js
+++ santhosh.js
@@ -1,17 +1,28 @@
 
-
 "use strict"
 
+let in_product=document.getElementById("in_product");
 
-let in_product=document.getElementById("in_product")
+let in_amount=document.getElementById("in_amount");
 
-let in_amount=document.getElementById("in_amount")
+let btn=document.getElementById("btn");
 
-let btn=document.getElementById("btn")
+btn.addEventListener("click",output);
 
-btn.addEventListener("click",output)
+let div1=document.createElement("list")
 
 function output()
 {
-    
+   alert.in_product=output;
+
+   alert.in_amount=output;
+
+   if(btn.alert=output)
+   {
+    btn.classList.add("list")
+
+    let div1=in_product.list;
+
+    let 
+   }
 }
